e33d52343bfbad425ea9eaf9a507faa2ab92eb3c
bijuaryal17@gmail.com <bijuaryal17@gmail.com>
2024-10-06 23:00:56 +0545

    Basic UI

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flappy/component/button.dart';
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatefulWidget {
@@ -10,6 +11,58 @@ class GameScreen extends StatefulWidget {
 class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {},
+        child: Stack(
+          children: [
+            // background
+            Positioned.fill(
+              child: Image.asset(
+                'assets/images/background-day.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            // Ground
+            Positioned(
+              bottom: 0,
+              child: Image.asset(
+                'assets/images/base.png',
+                width: MediaQuery.of(context).size.width,
+              ),
+            ),
+            // Bird
+            AnimatedContainer(
+              alignment: const Alignment(0, 0),
+              duration: const Duration(milliseconds: 0),
+              child: Image.asset(
+                'assets/images/bluebird-midflap.png',
+                width: 50,
+              ),
+            ),
+            // TODO: Pipes
+
+            // Score display
+            Positioned(
+              top: 50,
+              left: MediaQuery.of(context).size.width / 2 -
+                  25, // Center the score
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/7.png'),
+                  Image.asset('assets/images/9.png'),
+                ],
+              ),
+            ),
+
+            // Restart Game Button
+            const Center(
+              child: MyButton(text: "Restart"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Flappy Bird",
       home: GameScreen(),
     );
